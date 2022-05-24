4ce0d53703ff97c1cef91b23f90445610b450d64
JCarter426 <56983102+BBBrassil@users.noreply.github.com>
2022-05-24 04:03:55 -0400

    added layer switching prototype to allow player to go inside buildings

---
+++ Assets/Scripts/Entity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+	private Rigidbody2D body;
+
+	private Collider2D myCollider;
+
+	private SpriteRenderer spriteRenderer;
+
+	[Range(0f, short.MaxValue)]
+	private float moveSpeed = 10.0f;
+
+	void Awake() {
+		body = GetComponent<Rigidbody2D>();
+		myCollider = GetComponent<Collider2D>();
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	protected virtual void Start() {
+		Sort(Layers.FromId(gameObject.layer));
+	}
+
+	public void Move(Vector2 direction) {
+		body.velocity = moveSpeed * direction;
+
+	}
+
+	public void Move(Layers.Type layer) {
+		gameObject.layer = Layers.ToId(layer);
+		Sort(layer);
+	}
+
+	private void Sort(Layers.Type layer) {
+		Debug.Log($"current sorting layer: {spriteRenderer.sortingLayerID}");
+		Debug.Log($"changing to layer: {Layers.SortingLayerId(layer, Layers.Subtype.Static)}");
+		spriteRenderer.sortingLayerID = Layers.SortingLayerId(layer, Layers.Subtype.Dynamic);
+		Debug.Log($"new sorting layer: {spriteRenderer.sortingLayerID}");
+	}
+}
---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using UnityEngine;
+public static class Layers {
+	private const string DEFAULT = "Default";
+
+	private const int USER_MIN = 6;
+
+	public enum Type {
+		Default,
+		Universal,
+		Background,
+		Midground,
+		Foreground,
+	}
+
+	public enum Subtype {
+		Static,
+		Dynamic,
+	}
+
+	public static Type FromId(int id) {
+		return id < USER_MIN
+			? Type.Default
+			: (Type)(id - USER_MIN + 1);
+	}
+
+	public static int ToId(Type type) {
+		return type == Type.Default
+			? 0
+			: USER_MIN + (int)type - 1;
+	}
+
+	public static string NameOf(Type type) {
+		return Enum.GetName(typeof(Type), type);
+	}
+
+	public static string NameOf(Subtype subtype) {
+		return Enum.GetName(typeof(Subtype), subtype);
+	}
+
+	public static int LayerId(Type type) {
+		return LayerMask.NameToLayer(NameOf(type));
+	}
+
+	public static int SortingLayerId(Type type, Subtype subtype = Subtype.Static) {
+
+		Debug.Log($"given {type} ({(int)type})");
+
+		string name = type == Type.Default
+			? NameOf(type)
+			: NameOf(type) + "_" + NameOf(subtype);
+
+		Debug.Log($"name: {name}");
+		return SortingLayer.NameToID(name);
+	}
+
+	public static Type UpFrom(Type type) {
+		return type switch {
+			Type.Universal => Type.Universal,
+			Type.Background => Type.Midground,
+			Type.Midground => Type.Foreground,
+			Type.Foreground => Type.Foreground,
+			_ => Type.Default
+		};
+	}
+
+	public static Type DownFrom(Type type) {
+		return type switch {
+			Type.Universal => Type.Universal,
+			Type.Foreground => Type.Midground,
+			Type.Midground => Type.Background,
+			Type.Background => Type.Background,
+			_ => Type.Default
+		};
+	}
+}
