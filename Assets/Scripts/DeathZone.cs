46bec1614dc68b803f563a562327e3f12c5bb10f
JCarter426 <56983102+BBBrassil@users.noreply.github.com>
2022-05-30 01:50:33 -0400

    started building scene with final assets

--- Assets/Scripts/DeathZone.cs
+++ Assets/Scripts/DeathZone.cs
@@ -2,31 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathZone : MonoBehaviour
-{
-    private Collider2D collider2d;
-    void Awake()
-    {
-        collider2d = GetComponent<Collider2D>();
-    }
+public class DeathZone : MonoBehaviour {
+	private Collider2D collider2d;
+	void Awake() {
+		collider2d = GetComponent<Collider2D>();
+	}
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.TryGetComponent(out idleScript idle))
-        {
-            idle.Die();
-        }
-    }
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if( collision.TryGetComponent(out idleScript idle) ) {
+			idle.Die();
+		}
+	}
 
-    //editor only
-    void OnDrawGizmos()
-    {
-        if (collider2d == null)
-        {
-            collider2d = GetComponent<Collider2D>();
-        }
-        
-        Gizmos.color = Color.red;
-        Gizmos.DrawCube(transform.position, collider2d.bounds.size);
-    }
+	//editor only
+	void OnDrawGizmos() {
+		if( collider2d == null ) {
+			collider2d = GetComponent<Collider2D>();
+		}
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawCube(transform.position, collider2d.bounds.size);
+	}
 }
--- Assets/Scripts/tracker.cs
+++ Assets/Scripts/tracker.cs
@@ -2,48 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tracker : MonoBehaviour
-{
-    [SerializeField]
-    private GameObject playerObject;
-    [SerializeField]
-    private GameObject idlePlayerObject;
-    [SerializeField]
-    private Vector2 horizontalBounds;
-    private float baseSize = 5;
+public class tracker : MonoBehaviour {
+	[SerializeField]
+	private GameObject playerObject;
 
-    private void Start()
-    {
-        Camera.main.orthographicSize = baseSize;
-    }
+	[SerializeField]
+	private GameObject idlePlayerObject;
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 playerPos = playerObject.transform.position;
-        Vector2 idlePos = idlePlayerObject.transform.position;
+	[SerializeField]
+	private Vector2 horizontalBounds;
 
-        float x = (playerPos.x + idlePos.x) / 2;
-        if (Mathf.Abs(playerPos.y - idlePos.y) > Camera.main.orthographicSize * 2)
-        {
-            x = playerPos.x;
-        }
+	private float baseSize;
 
-        float halfWidth = Camera.main.orthographicSize * 1.8f;
-        if (x - halfWidth < horizontalBounds.x) // if leftmost point is less than x value of bounds (left bound)... 
-        {
-            x = horizontalBounds.x + halfWidth; // let x = x value of xBounds
-        } else if (x + Camera.main.orthographicSize * 1.8 > horizontalBounds.y) // if rightmost point is greater than y
-                                                                                // value of bounds (right bound)...
-        {
-            x = horizontalBounds.y - halfWidth; // let x = y value of xBounds
-        }
+	private void Start() {
+		baseSize = Camera.main.orthographicSize;
+	}
 
-        transform.position = new Vector3(x, 0, -10);
+	private float Center => playerObject.transform.position.y
+		+ (idlePlayerObject.transform.position.y - playerObject.transform.position.y) / 2;
 
-        if (Mathf.Abs(playerPos.x - idlePos.x) > 14)
-        {
-            Camera.main.orthographicSize = baseSize + 0.77f * (Mathf.Abs(playerPos.x - idlePos.x) - 14) / 2;
-        }
-    }
+	void Update() {
+		Vector2 playerPos = playerObject.transform.position;
+		Vector2 idlePos = idlePlayerObject.transform.position;
+
+		float x = (playerPos.x + idlePos.x) / 2;
+		if( Mathf.Abs(playerPos.y - idlePos.y) > Camera.main.orthographicSize * 2 ) {
+			x = playerPos.x;
+		}
+
+		float halfWidth = Camera.main.orthographicSize * 1.8f;
+		if( x - halfWidth < horizontalBounds.x ) // if leftmost point is less than x value of bounds (left bound)... 
+		{
+			x = horizontalBounds.x + halfWidth; // let x = x value of xBounds
+		}
+		else if( x + Camera.main.orthographicSize * 1.8 > horizontalBounds.y ) // if rightmost point is greater than y
+																			   // value of bounds (right bound)...
+	  {
+			x = horizontalBounds.y - halfWidth; // let x = y value of xBounds
+		}
+
+		transform.position = new Vector3(x, Center, -10);
+
+		if( Mathf.Abs(playerPos.x - idlePos.x) > 14 ) {
+			Camera.main.orthographicSize = baseSize + 0.77f * (Mathf.Abs(playerPos.x - idlePos.x) - 14) / 2;
+		}
+	}
 }
