284f3b21e5d67fd54dfee07954da99ddcf348cfc
JCarter426 <56983102+BBBrassil@users.noreply.github.com>
2022-05-29 20:24:27 -0400

    added sprites & animations for hydrant

--- Assets/Scripts/Geyser.cs
+++ Assets/Scripts/Geyser.cs
@@ -1,48 +1,82 @@
 using UnityEngine;
 
-public class Geyser : MonoBehaviour
-{
-    private Collider2D geyserCollider;
+public class Geyser : MonoBehaviour {
+	private Animator animator;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        Renderer rend = GetComponent<Renderer>();
-        rend.enabled = false;
+	private BoxCollider2D physicsCollider;
 
-        geyserCollider = GetComponent<Collider2D>();
-    }
+	[SerializeField]
+	private float maxHeight;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private float risingSpeed = .06f;
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Renderer rend = GetComponent<Renderer>();
+	private bool activated;
 
-        // if the idle player stays collided with the geyser and the geyser is enabled...
-        if (collision.gameObject.CompareTag("IdlePerson") && rend.enabled)
-        {
-            idleScript person = collision.GetComponent<idleScript>();
+	void Awake() {
+		physicsCollider = GetComponentInChildren<BoxCollider2D>();
+		animator = GetComponentInChildren<Animator>();
+		
+	}
 
-            // calculates the depth of the idle player
-            float playerHeight = collision.bounds.min.y;
-            float geyserHeight = geyserCollider.bounds.max.y;
-            float depth = geyserHeight - playerHeight;
+	void Start() {
+		Deactivate();
+	}
 
-            if(depth < .01)
-            {
-                // decreases speed of idle player
-                person.SlowDown();
-            }
-            else
-            {
-                // adds upwards force to idle player based on depth
-                person.Float(depth);
-            }
-        }
-    }
+	void Update() {
+		if( activated && physicsCollider.size.y < maxHeight )
+			Scale(risingSpeed);
+		if( !activated && physicsCollider.size.y > Mathf.Epsilon )
+			Scale(-risingSpeed);
+
+	}
+
+	private void Scale(float delta) {
+		physicsCollider.offset = new Vector2(
+			physicsCollider.offset.x,
+			delta / 2
+		);
+		physicsCollider.size = new Vector2(
+			physicsCollider.size.x,
+			physicsCollider.size.y + delta
+		);
+		// Physics2D.SyncTransforms();
+	}
+
+	void OnTriggerStay2D(Collider2D collision) {
+		Debug.Log($"{collision.gameObject.name} triggered");
+
+		// if the idle player stays collided with the geyser and the geyser is enabled...
+		if( activated && collision.gameObject.CompareTag("IdlePerson") ) {
+
+			Debug.Log("idle person is here");
+
+
+			idleScript person = collision.GetComponent<idleScript>();
+
+			// calculates the depth of the idle player
+			float playerHeight = collision.bounds.min.y;
+			float geyserHeight = physicsCollider.bounds.max.y;
+			float depth = geyserHeight - playerHeight;
+
+			if( depth < .01 ) {
+				// decreases speed of idle player
+				person.SlowDown();
+			}
+			else {
+				// adds upwards force to idle player based on depth
+				person.Float(depth);
+			}
+		}
+	}
+
+	public void Activate() {
+		activated = true;
+		animator.Play("fire_hydrant_start");
+	}
+
+	public void Deactivate() {
+		activated = false;
+		animator.Play("fire_hydrant_off");
+	}
 }
