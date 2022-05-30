b6f8bcfe698f88bef96ca2800c05984ea8c262e1
JCarter426 <56983102+BBBrassil@users.noreply.github.com>
2022-05-29 23:22:11 -0400

    added crane sprites

---
+++ Assets/Scripts/Cable.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Cable : MonoBehaviour {
+	[SerializeField]
+	private Transform a;
+
+	[SerializeField]
+	private Transform b;
+
+	private float Length => Mathf.Abs(a.position.y - b.position.y);
+
+	private float scale;
+
+	private void Awake() {
+		scale = transform.localScale.y / Length;
+	}
+
+	private void Update() {
+		transform.position = new Vector3(
+			transform.position.x,
+			a.position.y + (b.position.y - a.position.y) / 2,
+			transform.position.z
+		);
+		transform.localScale = new Vector3(
+			1,
+			scale * Length,
+			1
+		);
+	}
+}
